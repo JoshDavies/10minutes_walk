2cf0c207e84c6cb0a319d8b3694c488a22184497
Josh Davies <jdaviescreative@gmail.com>
2019-11-18 18:02:04 +0000

    added a new test for current coordinates

--- spec/walkApp_spec.rb
+++ spec/walkApp_spec.rb
@@ -1,5 +1,11 @@
 require 'walkApp'
 
 describe WalkApp do
-  
+  let(:person) { WalkApp.new }
+
+  describe 'current_coordinates' do
+    it 'returns the persons current coordinates' do
+      expect(person.current_coordinates).to eq(0,0)
+    end
+  end
 end
