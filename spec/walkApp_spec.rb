e1abff2f98b6c0c80b0c5d088b95996192f150b3
Josh Davies <jdaviescreative@gmail.com>
2019-11-18 18:16:51 +0000

    setup test for go north

--- spec/walkApp_spec.rb
+++ spec/walkApp_spec.rb
@@ -8,4 +8,10 @@ describe WalkApp do
       expect(person.current_coordinates).to eq([0,0])
     end
   end
+  describe 'go_north' do
+    it 'changes the persons y coordinates positively' do
+      person.go_north
+      expect(person.current_coordinates).to eq([0,1])
+    end
+  end
 end
